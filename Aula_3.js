a70c70d83e749802cf1a03a1ffa57eabc8ff0578
yasmim-andrade30 <yasmimandrade1104@gmail.com>
2026-06-10 18:08:30 -0300

    Add files via upload

---
+++ Aula_3.js
@@ -0,0 +1,31 @@
+let nomeAluno = "Yasmim ";
+
+console.log(nomeAluno)
+
+let idade = 18;
+
+console.log("Olá " + nomeAluno + "Idade " + idade)
+
+let saudacao = "Olá " + nomeAluno + "Como está?"
+
+console.log(saudacao)
+
+if (idade >= 18) {
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Voce é menor de idade.")
+}
+//uma compettição de natação
+//se a idade for menor que 12 anos = Competição infantil
+//se a idade for entre 12 e 15 anos = competicção infanto-juvenil
+//se aidade for entre 15 a 17 anos = competição juvenil
+//se a idade for maios de 18 aanos = competição adulta
+//você insira a idade do competidor e o nome e o programa imprima
+//fulano, você participará da competição XXXX
+
+let nomedocompetidor = "Yasmim";
+idade = 11;
+if(idade < 12){
+    console.log(nomedocompetidor + ", você participará da Competição Infantil")}
+
+
---
+++ Aula_4.js
@@ -0,0 +1,18 @@
+
+
+
+
+let idade
+idade = 11;
+if (idade <= 12)
+
+
+    if (idade <= 12)
+        preco = 15.00
+if (idade <= 12 && idade <= 17) {
+    preco = 20.00
+} else {
+    preco = 40.00
+}
+
+console.log("Voce tem " + idade + " seu ingresso custa:!.")
